9c42cf110b5d0c6e41cbad5740f5332e9a2d8061
biplavs <biplavs@us.watson.ibm.com>
2021-12-14 01:07:53 +0530

    initial

---
+++ sample-code/Class1-HelloWorld/HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   /* my first program in C */
+   printf("Hello, World! \n");
+
+   return 0;
+}
+
---
+++ sample-code/Class1-HelloWorld/Readme.txt
@@ -0,0 +1,4 @@
+Instructions to run:
+
+1. gcc -v HelloWorld.c -o Hello
+2. ./Hello
